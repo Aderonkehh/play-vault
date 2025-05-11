8b18ce748fb1b181355a711142af26fa7038ef8c
Aderonke <aderonke@MacBook-Air.local>
2025-05-10 21:39:08 -0700

    delete account

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'sruthi',  
+  password: 'Aderonke@01',  
   database: 'projectdb'  
 });
 
