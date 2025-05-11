fc223e3d9899a36fa980f33721e01f94cdc797ee
Aderonke <aderonke@MacBook-Air.local>
2025-05-11 11:17:36 -0700

    final final final. NO MORE CHANGES

--- frontend/scripts/gameLaunchPage.js
+++ frontend/scripts/gameLaunchPage.js
@@ -56,7 +56,7 @@ async function setNewPassword() {
 
   try {
     const res = await fetch('/reset-password', {
-      method: 'PUT',
+      method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, newPassword })
     });
